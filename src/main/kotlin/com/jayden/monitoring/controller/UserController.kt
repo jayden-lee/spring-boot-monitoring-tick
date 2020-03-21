90c19b8d0c9c64fb241e85ea5a562a3e19c51632
jayden-lee <gwanggi.lee@gmail.com>
2020-03-22 01:52:37 +0900

    Implement service and controller

--- src/main/kotlin/com/jayden/monitoring/controller/UserController.kt
+++ src/main/kotlin/com/jayden/monitoring/controller/UserController.kt
@@ -1,12 +1,37 @@
 package com.jayden.monitoring.controller
 
+import com.jayden.monitoring.domain.User
 import com.jayden.monitoring.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 
 @RestController
+@RequestMapping("/v1/users")
 class UserController {
 
     @Autowired
     lateinit var userService: UserService
+
+    @PostMapping
+    fun createUser(@RequestBody request: CreateUserRequest) : Long {
+        return userService.createUser(request.name)
+    }
+
+    @GetMapping
+    fun getUserList() : List<User> {
+        return userService.getUserList()
+    }
+
+    @GetMapping("/{userId}")
+    fun getUser(@PathVariable id: Long) : User {
+        return userService.getUser(id)
+    }
 }
+
+data class CreateUserRequest(
+    @NotEmpty
+    @NotNull
+    val name: String
+)
